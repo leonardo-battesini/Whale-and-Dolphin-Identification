43bcc893c55de86896d7e64768ed23aa86f75efc
Leonardo Battesini <leonardoffline@gmail.com>
2022-02-22 19:31:47 -0300

    add IdentificaTipoPeixe()

--- util/CriaTagTipo.py
+++ util/CriaTagTipo.py
@@ -4,10 +4,10 @@ import logging
 import pandas as pd
 
 class CriaTagTipo:
-    def CriaTagTipo():
+    def GeraCsvComTipo():
 
         """
-        recebe CSV e identifica golfinho ou baleia.
+        recebe CSV e gera outro especificando a flg: golfinhos ou baleias.
         """
 
         logging.info('Started CriaTagTipo')
@@ -28,17 +28,28 @@ class CriaTagTipo:
             writer.writeheader()
 
             for i in range(len(df.index)):
-                if "whale" in df.loc[i][1] or "beluga" in df.loc[i][1] or "globis" in df.loc[i][1]:
-                    type = "whale"
-                elif "dolphin" in df.loc[i][1] or "dolpin" in df.loc[i][1]:
-                    type = "dolphin"
-                else:
-                    type = "ERROR"
-                    logging.warning('Type ' + df.loc[i][1] + 'not found!')
+
+                type = CriaTagTipo.IdentificaTipoPeixe(df.loc[i][1])
+
                 writer.writerow({'image': df.loc[i][0], 'species': df.loc[i][1], 'type': type, 'individual_id': df.loc[i][2]})
 
         csvfile.close()
 
         logging.info('Finished CriaTagTipo')
 
-CriaTagTipo.CriaTagTipo()
+    def IdentificaTipoPeixe(str):
+
+        """
+        identifica pelo nome da espécie se é golfinhos ou baleias.
+        """
+
+        if "whale" in str or "beluga" in str or "globis" in str:
+            type = "whale"
+        elif "dolphin" in str or "dolpin" in str:
+            type = "dolphin"
+        else:
+            type = "ERROR"
+            logging.warning('Type ' + str + 'not found!')
+        return(type)
+
+#CriaTagTipo.GeraCsvComTipo()
