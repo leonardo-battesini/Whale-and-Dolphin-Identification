c0bfd2dea78b930464eaa374406cb0492a3ec1de
daniel_orlandi_climatempo <daniel.orlandi@climatempo.com.br>
2022-03-11 17:19:29 -0300

    Add run method to CriaTagTipo

--- util/CriaTagTipo.py
+++ util/CriaTagTipo.py
@@ -2,9 +2,24 @@ import csv
 import os
 import logging
 import pandas as pd
+import platform
 
 class CriaTagTipo:
-    def GeraCsvComTipo():
+    def run():
+        """
+        Identifica o os e ajusta a sintaxe dos paths .
+        """
+        os_name = platform.system()
+        print(os_name)
+        if ((str(os_name) != '5.10.60.1-microsoft-standard-WSL2') and (str(os_name) != 'Linux')):
+            TRAINING_CSV = '\\happy-whale-and-dolphin\\train.csv'
+            OUTPUT_DIR = '\\outputs\\'
+            OUTPUT_FILE = '\\train_with_tags.csv'
+        
+        else:
+            TRAINING_CSV = '/happy-whale-and-dolphin/train.csv'
+            OUTPUT_DIR = '/outputs/'
+            OUTPUT_FILE = '/train_with_tags.csv'
 
         """
         recebe CSV e gera outro especificando a flg: golfinhos ou baleias.
@@ -12,11 +27,11 @@ class CriaTagTipo:
 
         logging.info('Started CriaTagTipo')
 
-        df = pd.read_csv(os.path.abspath(os.curdir) +'\\train.csv')
+        df = pd.read_csv(os.path.abspath(os.curdir) + TRAINING_CSV)
 
         root_dir = os.path.abspath(os.curdir)
-        destination_dir = root_dir + '\\outputs\\'
-        csv_dir = destination_dir +'\\train_with_tags.csv'
+        destination_dir = root_dir + OUTPUT_DIR
+        csv_dir = destination_dir + OUTPUT_FILE
 
         if not os.path.os.path.exists(destination_dir):
             os.makedirs(destination_dir)
@@ -37,19 +52,18 @@ class CriaTagTipo:
 
         logging.info('Finished CriaTagTipo')
 
-    def IdentificaTipoPeixe(str):
+def IdentificaTipo(nome):
+    """
+    Lê o nome da espécie e devolve o tipo.
+    """
 
-        """
-        identifica pelo nome da espécie se é golfinhos ou baleias.
-        """
+    if "whale" in nome or "beluga" in nome or "globis" in nome:
+        type = "whale"
+    elif "dolphin" in nome or "dolpin" in nome:
+        type = "dolphin"
+    else:
+        type = "ERROR"
+        logging.warning('Type of' + nome + ' not found!')
+    return type
 
-        if "whale" in str or "beluga" in str or "globis" in str:
-            type = "whale"
-        elif "dolphin" in str or "dolpin" in str:
-            type = "dolphin"
-        else:
-            type = "ERROR"
-            logging.warning('Type ' + str + 'not found!')
-        return(type)
 
-#CriaTagTipo.GeraCsvComTipo()
